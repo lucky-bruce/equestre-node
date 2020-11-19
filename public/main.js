eeca3bb48d23bd794e9e76ddae3fa65fc09b8f3b
lucky-bruce <jalicorich@gmail.com>
2020-11-19 23:03:08 +0300

    fix: date error in safari

--- public/main.js
+++ public/main.js
@@ -456,7 +456,7 @@ $(function () {
     }
 
     function formatDate(dateString) {
-        var d = new Date(dateString);
+        var d = new Date(dateString.replace(/\s/, 'T'));
 
         return ("0" + d.getDate()).slice(-2) + "." + ("0"+(d.getMonth()+1)).slice(-2) + "." + d.getFullYear();
     }
